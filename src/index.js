249b0e0cd873530cb2211d872f163a7dfe5ab6cc
Akash <aakashjha97@gmail.com>
2020-11-01 20:34:52 +0530

    ready to deploy

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { RoomProvider } from "./context";
+import { BrowserRouter } from "react-router-dom";
+
+
+
+ReactDOM.render(
+  <RoomProvider>
+    <BrowserRouter>
+      <React.StrictMode>
+    <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </RoomProvider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
